0434c14e4f78faaea740f2386290cb8b00b79f13
汤胜-01401957 <yupaits@sfmail.sf-express.com>
2020-12-08 15:10:56 +0800

    插入排序示例

--- java-se/src/main/java/algorithm/sort/InsertSortSample.java
+++ java-se/src/main/java/algorithm/sort/InsertSortSample.java
@@ -7,16 +7,18 @@ import java.util.Arrays;
  * @author ts495
  * @date 2020/9/16
  */
-public class SortTest {
+public class InsertSortSample {
     public static void insertSort(int[] array) {
         for (int i = 2; i < array.length; i++) {
             int val = array[i];
             int j = i - 1;
-            while (j >= 0 && array[j] > val) { // array[j] > val
+            // array[j] > val
+            while (j >= 0 && array[j] > val) {
                 array[j + 1] = array[j];
                 j--;
             }
-            array[j + 1] = val; // array[j + 1]不是array[j]
+            // array[j + 1]不是array[j]
+            array[j + 1] = val;
         }
     }
 
